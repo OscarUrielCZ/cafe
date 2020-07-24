197dfbec4b49ed40f3df6d9e7645952441a4cd32
Oscar Uriel CZ <oscar2410.oucz@gmail.com>
2020-07-24 01:56:24 -0500

    Cambios a las rutas

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const _ = require('underscore');
 
 const User = require('../models/user');
@@ -34,9 +34,11 @@ app.get('/users', (req, res) => {
         });
 });
 
+// Create a new user
 app.post('/user', (req, res) => {
     const body = req.body;
 
+    console.log(body);
     const user = new User({
         name: body.name,
         email: body.email,
